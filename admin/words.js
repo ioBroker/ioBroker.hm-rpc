c6345df14969132504b5c64ff779de701fe94d1f
GermanBluefox <dogafox@gmail.com>
2017-06-10 21:23:54 +0200

    (bluefox) small changes for callback address

---
+++ admin/words.js
@@ -0,0 +1,28 @@
+systemDictionary = {
+    "HomeMatic Address":        {"en": "HomeMatic Address",        "de": "HomeMatic Adresse",        "ru": "Адрес HomeMatic"},
+    "Adapter Address":          {"en": "Adapter Address",          "de": "Adapter Adresse",          "ru": "Собственный адрес"},
+    "Daemon":                   {"en": "Daemon",                   "de": "Daemon",                   "ru": "Служба"},
+    "HomeMatic Port":           {"en": "HomeMatic Port",           "de": "HomeMatic Port",           "ru": "Порт HomeMatic"},
+    "Adapter Callback Address": {"en": "Adapter Callback Address", "de": "Adapter Callback Adresse", "ru": "Собственный Callback адрес"},
+    "Adapter Port":             {"en": "Adapter Port",             "de": "Adapter Port",             "ru": "Порт драйвера ioBroker"},
+    "Protocol":                 {"en": "Protocol",                 "de": "Protokoll",                "ru": "Протокол"},
+    "Check communication interval (sec)":   {"en": "Check communication interval(sec)", "de": "Verbindungs-Check Interval(sek)", "ru": "Интервал проверки соединения(сек)"},
+    "Reconnect interval (sec)": {"en": "Reconnect interval (sec)", "de": "Wiederverbindungs-Interval(sek)", "ru": "Интервал повтора соединения(сек)"},
+    "Listen on all IPs":        {"en": "Listen on all IPs",        "de": "Auf alle IPs hören",       "ru": "Listen on all IP"},
+    "Homematic RPC adapter settings": {
+        "en": "Homematic RPC adapter settings",
+        "de": "Homematic RPC Adapter Einstellungen",
+        "ru": "Настройки Homematic RPC драйвера"
+    },
+    "This IP is unreachable from %s": {
+        "en": "This IP is unreachable from %s",
+        "de": "Diese IP ist nicht erreichbar von %s",
+        "ru": "Этот IP не доступен для %s"
+    },
+    "Please specify callback address": {
+        "en": "Please specify callback address.",
+        "de": "Bitte callback IP-Adresse eingeben.",
+        "ru": "Введите IP адрес для callback."},
+    "Ok": {"en": "Ok", "de": "Ok", "ru": "Ok"},
+    "Force objects re-init": {"en": "Synchronize objects (once)", "de": "Synchronisiere Geräte neu (einmalig)",  "ru": "Синхронизировать объекты (один раз)"}
+};
