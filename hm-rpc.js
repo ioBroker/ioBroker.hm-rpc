2ab9fa350e39c4c876b10e996ca22d9c8b2b4fee
angelnu <github@angelnu.com>
2017-06-10 17:00:29 +0200

    Added Callback address - needed for Docker

--- hm-rpc.js
+++ hm-rpc.js
@@ -677,8 +677,9 @@ function initRpcServer() {
 
     //adapterPort was introduced in v1.0.1. If not set yet then try 2000
     var adapterPort = parseInt(adapter.config.port || adapter.config.homematicPort, 10) || 2000;
+    var callbackAddress = adapter.config.callbackAddress || adapter.config.adapterAddress;
     adapter.getPort(adapterPort, function (port) {
-        daemonURL = daemonProto + adapter.config.adapterAddress + ':' + port;
+        daemonURL = daemonProto + callbackAddress + ':' + port;
         rpcServer = rpc.createServer({host: adapter.config.adapterAddress, port: port});
 
         adapter.log.info(adapter.config.type + 'rpc server is trying to listen on ' + adapter.config.adapterAddress + ':' + port);
