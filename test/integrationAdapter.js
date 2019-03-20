01dd6eed1281f9c2847789b49c9effe7b97918ce
Moritz Heusinger <moritz.heusinger@gmail.com>
2019-03-20 16:16:22 +0100

    adjust tests ..

--- test/integrationAdapter.js
+++ test/integrationAdapter.js
@@ -5,13 +5,6 @@ const {tests} = require('@iobroker/testing');
 
 // Run tests
 tests.integration(path.join(__dirname, '..'), {
-    //            ~~~~~~~~~~~~~~~~~~~~~~~~~
-    // This should be the adapter's root directory
-
-    // If the adapter may call process.exit during startup, define here which exit codes are allowed.
-    // By default, termination during startup is not allowed.
-    allowedExitCodes: [11],
-
     defineAdditionalTests(getHarness) {
 
         describe('Test sendTo()', () => {
