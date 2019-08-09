8577033daf060685c63264fa4af517f7eed94f5e
Max Hauser <moritz.heusinger@gmail.com>
2019-08-09 09:33:39 +0200

    io-packge json is growing to big
    - for ci test server
    
    - also it probably does not pay off to store all the meta data in io-pack to reduce number of rpc paramset calls
    
    - we are now logging on info and rm call for action
    - we should only add metadata if metadata needs manual correction

--- hm-rpc.js
+++ hm-rpc.js
@@ -1181,8 +1181,8 @@ function getValueParamsets() {
                                 // if not empty
                                 for (const attr in res) {
                                     if (res.hasOwnProperty(attr)) {
-                                        adapter.log.warn(`Send this info to developer: "_id": "${key}"`);
-                                        adapter.log.warn(`Send this info to developer: ${JSON.stringify(paramset)}`);
+                                        adapter.log.debug(`New metadata added: "_id": "${key}"`);
+                                        adapter.log.debug(`New metadata added: ${JSON.stringify(paramset)}`);
                                         break;
                                     }
                                 }
